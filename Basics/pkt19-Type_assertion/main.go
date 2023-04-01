b4e4bd56c6b50b8b27a0046de5592fe37c2e8887
Rodrigo Latorraca <rlatorraca@gmail.com>
2023-04-01 11:50:31 -0300

    Type assertion

---
+++ Basics/pkt19-Type_assertion/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* Type Assertion */
+
+func main() {
+
+	var name interface{} = "John Marshall"
+	var number interface{} = 100.00
+
+	println(name)
+	println(name.(string))
+
+	result, ok := name.(int)
+	fmt.Printf("Result: %v | Ok: %v\n", result, ok)
+
+	println(number)
+	println(number.(float64))
+
+	result, ok = number.(int)
+	fmt.Printf("Result: %v | Ok: %v", result, ok)
+
+}
