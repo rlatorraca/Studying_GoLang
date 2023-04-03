cfdb438b450a57a7df8779742be218a79f5c50c9
Rodrigo Latorraca <rlatorraca@gmail.com>
2023-04-03 12:57:06 -0300

    Packages and modules done

--- Basics/pkt21-Packages_and_modules_01/main.go
+++ Basics/pkt21-Packages_and_modules_01/main.go
@@ -10,4 +10,10 @@ func main() {
 
 	fmt.Printf("Result: %v", s)
 
+	car := math.Car{Make: "Honda", Model: "Accord Touring"}
+	fmt.Printf("Result: %v\n", s)
+	fmt.Printf("Car: %v | %v\n", car.Make, car.Model)
+	fmt.Printf("%v\n", math.A) // atributo criado dentro do arquivo math/math.go
+	fmt.Printf("%v", car.Run())
+
 }
--- Basics/pkt21-Packages_and_modules_01/math/math.go
+++ Basics/pkt21-Packages_and_modules_01/math/math.go
@@ -1,5 +1,19 @@
 package math
 
+var A int = 10
+var a int = 15
+
+type Car struct {
+	Make  string
+	Model string
+}
+
+func (c Car) Run() string {
+	return "Car running"
+}
+
+// Sum
+/* o nome da funcao precisar estar em MAISCULO para ser vista de FORA do pacote*/
 func Sum[T int | float64](x, y T) T {
 	return x + y
 }
