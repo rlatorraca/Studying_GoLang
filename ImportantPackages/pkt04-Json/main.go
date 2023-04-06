99c232f6c1a41f2a0e316fb479522001a1a32f37
Rodrigo Latorraca <rlatorraca@gmail.com>
2023-04-05 23:04:31 -0300

    JSON in GoLang

--- ImportantPackages/pkt04-Json/main.go
+++ ImportantPackages/pkt04-Json/main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
+	"os"
 )
+
+type Account struct {
+	AccountNumber int     `json:"num"` // tag
+	Balance       float64 `json:"bal" validate:"max=5000"`
+}
 
 /* Manipulando Arquivos em GoLang*/
 func main() {
 
-	request, err := http.Get("https://www.terra.com.br")
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(request.Body)
-
+	// MANEIRA SIMPLES => Dentro de um variavel
+	account := Account{AccountNumber: 12312, Balance: 11231.33}
+	request, err := json.Marshal(account)
 	if err != nil {
-		log.Fatal(fmt.Printf("[ERROR] Getting site... : %v\n", err))
+		log.Fatal("[ERROR] Reading JSON...")
 	}
 
-	response, err := io.ReadAll(request.Body)
+	fmt.Println(string(request))
+
+	// RECEBE o valor e faz o ENCODER para gravar em outro lugar (arquivo, tela)
+	err = json.NewEncoder(os.Stdout).Encode(account)
 	if err != nil {
-		log.Fatal(fmt.Printf("[ERROR] Reading Site Body... : %v\n", err))
+		log.Fatal("[ERROR] ENCODE JSON...")
 	}
 
-	fmt.Printf(string(response))
+	// Transformar JSON em Struct
+	jsonRaw := []byte(`{"num": 333, "bal": 34343.44}`)
+	var account2 Account
+	err = json.Unmarshal(jsonRaw, &account2)
+	if err != nil {
+		return
+	}
 
+	fmt.Printf("Account: %v | Balance: %v", account2.AccountNumber, account2.Balance)
 }
