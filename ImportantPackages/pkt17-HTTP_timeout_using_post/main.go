28a876bdb99a55f6d7b628004bc75bb91142f8ff
rlatorraca <rlatorraca@gmail.com>
2023-04-15 23:42:46 -0300

    HTTP using POST example

--- ImportantPackages/pkt17-HTTP_timeout_using_post/main.go
+++ ImportantPackages/pkt17-HTTP_timeout_using_post/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"io"
 	"log"
 	"net/http"
@@ -11,23 +10,18 @@ import (
 /* Manipulation Arquivos em GoLang*/
 func main() {
 
-	callHTTP := http.Client{Timeout: 2 * time.Second} // 2 segundos é o tempo de espera
-
-	json := bytes.NewBuffer([]byte(`{"nome":"Lucas"}{"nome":"Joao"}{"nome":"Marcos"}`))
-
-	/* Usando POST */
-	response, err := callHTTP.Post("https://www.uol.com.br", "application/json", json)
+	//callHTTP := http.Client{Timeout: 2 * time.Second} // 2 segundos é o tempo de espera
+	callHTTP := http.Client{Timeout: 2 * time.Microsecond} // 2 micro-segundos é o tempo de espera
+	response, err := callHTTP.Get("https://www.google.com")
 	if err != nil {
 		log.Fatal("[ERROR] Timeout Exceeded ...", err)
 	}
-
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 			log.Fatal("[ERROR] Closing body ...", err)
 		}
 	}(response.Body) // fecha o body da resposta
-
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal("[ERROR] Reading body ...", err)
